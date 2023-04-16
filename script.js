1ffe5cd5ed8b4efd02a62f91f17af579fa66dec4
4theyouth <114873096+4theyouth@users.noreply.github.com>
2023-04-16 21:46:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const setName = () => (document.getElementById('user-name').innerText = prompt('인스타그램 아이디가 무엇인가요?'))
+const setName = () => {
+  const newname = prompt('인스타그램 아이디가 무엇인가요?')
+  
+  if(!newname) return alert("값이 입력되지 않았습니다.")
+  document.getElementById('user-name').innerText = newname
+}
